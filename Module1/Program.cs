5c9342f6247936faa05214346dc4bac50687b0f0
Marcus Johnson <marx5185@gmail.com>
2025-06-23 01:59:14 -0500

    added a method to find the area of a triangle and prompted the used to give the base and hight of the triangle

--- Module1/Program.cs
+++ Module1/Program.cs
@@ -6,11 +6,23 @@ class Program
     {
         Console.WriteLine ("What is the radius of the circle");
         var radius = Console.ReadLine(); 
-        Console.WriteLine( CArea (Convert.ToDouble (radius)));
+        Console.WriteLine( "The area is "+ CArea (Convert.ToDouble (radius)));
+        
+        Console.WriteLine ("What is the base of the triangle");
+        var baseLength = Console.ReadLine();
+        Console.WriteLine ("What is the hight of the triangle");
+        var hight = Console.ReadLine();
+        Console.WriteLine("The area is " + TArea (Convert.ToDouble(baseLength),Convert.ToDouble(hight)));
     }
     static double CArea(double radius)
     {
         return Math.PI * radius * radius;
        
     }
+    static double TArea(double baseLength, double hight)
+    {
+        return baseLength * hight / 2;
+        
+    }
+  
 }

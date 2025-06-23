a238d6e739ad35e2442b5186b7ba5d45b9942c4b
Marcus Johnson <marx5185@gmail.com>
2025-06-23 01:42:02 -0500

    Created a method that gets the area of a circle and a prompt for the user to input the radius of the circle

---
+++ Module1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Module1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine ("What is the radius of the circle");
+        var radius = Console.ReadLine(); 
+        Console.WriteLine( CArea (Convert.ToDouble (radius)));
+    }
+    static double CArea(double radius)
+    {
+        return Math.PI * radius * radius;
+       
+    }
+}

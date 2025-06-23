38a2479b8450bf85a7df91c02ff8cbb6812dba71
Marcus Johnson <marx5185@gmail.com>
2025-06-23 02:34:36 -0500

    added the ability for the user to choose which shape they wanted to find the area for

--- Module1/Program.cs
+++ Module1/Program.cs
@@ -4,25 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine ("What is the radius of the circle");
-        var radius = Console.ReadLine(); 
-        Console.WriteLine( "The area is "+ CArea (Convert.ToDouble (radius)));
+        Console.WriteLine ("Which shapes area do you need to find");
+        Console.WriteLine ("Enter the number next to the shape to choose");
+        Console.WriteLine ("Circle 1");
+        Console.WriteLine ("Trangle 2");
+        Console.WriteLine ("Rectangle 3");
+        Console.WriteLine ("Square 4");
+        var answer = Console.ReadLine();
+
+
+        if ( answer == "1")
+        {
+            Console.WriteLine ("What is the radius of the circle");
+            var radius = Console.ReadLine(); 
+            Console.WriteLine( "The area is "+ CArea (Convert.ToDouble (radius)));
+        }
+  
+        if ( answer == "2")
+        {
+            Console.WriteLine ("What is the base of the triangle");
+            var baseLength = Console.ReadLine();
+            Console.WriteLine ("What is the hight of the triangle");
+            var hight = Console.ReadLine();
+            Console.WriteLine("The area is " + TArea (Convert.ToDouble(baseLength),Convert.ToDouble(hight)));
+        }
         
-        Console.WriteLine ("What is the base of the triangle");
-        var baseLength = Console.ReadLine();
-        Console.WriteLine ("What is the hight of the triangle");
-        var hight = Console.ReadLine();
-        Console.WriteLine("The area is " + TArea (Convert.ToDouble(baseLength),Convert.ToDouble(hight)));
+        if ( answer == "3")
+        {
+            Console.WriteLine ("What is the length of the rectangle");
+            var length = Console.ReadLine();
+            Console.WriteLine ("What is the width of the rectangle");
+            var width = Console.ReadLine();
+            Console.WriteLine("The area is " + RArea (Convert.ToDouble(length),Convert.ToDouble(width)));
+        }
         
-        Console.WriteLine ("What is the length of the rectangle");
-        var length = Console.ReadLine();
-        Console.WriteLine ("What is the width of the rectangle");
-        var width = Console.ReadLine();
-        Console.WriteLine("The area is " + RArea (Convert.ToDouble(length),Convert.ToDouble(width)));
-        
+        if ( answer == "4")
+        {
         Console.WriteLine ("How long are the sides");
         var side = Console.ReadLine(); 
         Console.WriteLine( "The area is "+ SArea (Convert.ToDouble (side)));
+        }
     }
     static double CArea(double radius)
     {
